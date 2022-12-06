43c9bf97c7d77990dfa2a9e3fad2b0f0bd2a4dc6
Yacin Abdallah <yacin.bakir@gmail.com>
2022-12-06 16:40:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.web-sg.id}"]
+  vpc_security_group_ids = "vpc-00a82efcb28712659"
 
   user_data = <<-EOF
               #!/bin/bash

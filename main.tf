5379958b4240e75157f2e80c41852b9eee48481d
Yacin Abdallah <yacin.bakir@gmail.com>
2022-12-06 16:36:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
+  name = "segtest-sg"
   ingress {
     from_port   = 8080
     to_port     = 8080
